5ab0ccfc9ad282c5f1ceded14cbc9c4ec41d3193
CashregisterSH <ibrahim.workconnect@gmail.com>
2025-03-13 18:10:01 +0000

    lokesh bhai

--- Reminder/reminders.js
+++ Reminder/reminders.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Reminder = require('../Reminder/Reminder');
 const Notification = require('../Reminder/Notification');
 const { sendEmail } = require('../Reminder/email');
-const { getUsersByOrganization } = require('..//Reminder/user');
+const { getUsersByOrganization } = require('../Reminder/user');
 const { verifyToken } = require('../LoginSignup/auth.middleware');
 
 // GET /reminders - list reminders for the user's organization
